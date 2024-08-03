1ae4a21499a2cd6dd140362077baff94a61519fe
mamad <khoshkhab11@gmail.com>
2024-08-03 19:08:50 +0330

    further implementation of rudimentary functions

--- local/dir.go
+++ local/dir.go
@@ -3,14 +3,17 @@ package local
 import (
 	"context"
 	"os"
+	"path/filepath"
 	"sync"
+	"syscall"
 
 	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
 )
 
 type FDir struct {
 	CNode
-	Children *map[string]*Ffile
+	Children *map[string]fs.Node
 	Lock     *sync.Mutex
 }
 
@@ -20,5 +23,95 @@ func (fd FDir) Attr(ctx context.Context, a *fuse.Attr) error {
 		return err
 	}
 	a.Mode |= os.ModeDir
+	return nil
+}
+
+func (fd FDir) LookUp(ctx context.Context, name string) (fs.Node, error) {
+	if file, ok := (*fd.Children)[name]; ok {
+		return file, nil
+	}
+	return nil, syscall.ENONET
+}
+func (fd *FDir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
+	fd.Lock.Lock()
+	defer fd.Lock.Unlock()
+
+	name := filepath.Join(fd.Path, req.Name)
+
+	if d, err := fd.LookUp(ctx, name); d != nil && err == nil {
+		return nil, syscall.EEXIST
+	}
+
+	children := make(map[string]fs.Node)
+
+	err := os.Mkdir(name, req.Mode)
+	if err != nil {
+		return FDir{}, err
+	}
+
+	dir := &FDir{
+		CNode: CNode{
+			Inode: fs.GenerateDynamicInode(fd.Inode, req.Name),
+			Path:  name,
+		},
+		Children: &children,
+		Lock:     &sync.Mutex{},
+	}
+
+	(*fd.Children)[req.Name] = dir
+	return dir, nil
+}
+
+func (fd *FDir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error) {
+	fd.Lock.Lock()
+	defer fd.Lock.Unlock()
+
+	name := filepath.Join(fd.Path, req.Name)
+
+	if d, err := fd.LookUp(ctx, name); d != nil && err == nil {
+		return nil, nil, syscall.EEXIST
+	}
+
+	file, err := os.OpenFile(name, int(req.Flags), req.Mode)
+	if err != nil {
+		return nil, nil, err
+
+	}
+	ffile := Ffile{
+		CNode: CNode{
+			Inode: fs.GenerateDynamicInode(fd.Inode, req.Name),
+			Path:  name,
+		},
+		Data: []byte{},
+		Mode: req.Mode,
+	}
+	(*fd.Children)[req.Name] = ffile
+	return ffile, file, nil
+
+}
+
+func (fd *FDir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
+	fd.Lock.Lock()
+	defer fd.Lock.Unlock()
+
+	if _, ok := (*fd.Children)[req.Name]; !ok {
+		return syscall.ENONET
+	}
+
+	name := filepath.Join(fd.Path, req.Name)
+
+	var err error
+	if req.Dir {
+		err = os.RemoveAll(name)
+	} else {
+		err = os.Remove(req.Name)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	delete(*fd.Children, req.Name)
+
 	return nil
 }
--- local/file.go
+++ local/file.go
@@ -2,7 +2,9 @@ package local
 
 import (
 	"context"
+	"io"
 	"os"
+	"sync"
 
 	"bazil.org/fuse"
 )
@@ -11,6 +13,7 @@ type Ffile struct {
 	CNode
 	Data []byte
 	Mode os.FileMode
+	Lock *sync.Mutex
 }
 
 func (ff Ffile) Attr(ctx context.Context, a *fuse.Attr) error {
@@ -18,5 +21,71 @@ func (ff Ffile) Attr(ctx context.Context, a *fuse.Attr) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (ff Ffile) ReadAll(ctx context.Context) ([]byte, error) {
+	return os.ReadFile(ff.Path)
+}
+
+func (ff Ffile) Read(ctx context.Context, req *fuse.ReadRequest, res *fuse.ReadResponse) error {
+	file, err := os.Open(ff.Path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Seek(req.Offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	res.Data = make([]byte, req.Size)
+	n, err := file.Read(res.Data)
+	res.Data = res.Data[:n]
+	return err
+}
+
+func (ff Ffile) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
+	ff.Lock.Lock()
+	defer ff.Lock.Unlock()
+
+	file, err := os.OpenFile(ff.Path, os.O_WRONLY, ff.Mode)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	n, err := file.WriteAt(req.Data, req.Offset)
+	if err != nil {
+		return err
+	}
+	resp.Size = n
+	
+	return nil
+}
+
+func (ff Ffile) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) error {
+	if req.Valid.Size() {
+		err := os.Truncate(ff.Path, int64(req.Size))
+		if err != nil {
+			return err
+		}
+	}
+	if req.Valid.Gid() || req.Valid.Uid() {
+		err := os.Chown(ff.Path, int(req.Uid), int(req.Gid))
+		if err != nil {
+			return err
+		}
+	}
+	if req.Valid.Mtime() {
+		err := os.Chtimes(ff.Path, req.Atime, req.Mtime)
+		if err != nil {
+			return err
+		}
+	}
+
+	//TODO: Update Response
+
 	return nil
 }
